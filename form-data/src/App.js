2c774eef3dd132553a413e10f88969c37f600ed9
Narendra Mahajan <129751928+narendra1378@users.noreply.github.com>
2024-08-30 14:15:22 +0530

    Update App.js

--- form-data/src/App.js
+++ form-data/src/App.js
@@ -1,7 +1,7 @@
 // src/App.js
 import React from "react";
 import RegistrationForm from "./components/RegistrationForm";
-
+import "./RegistrationForm.css"
 function App() {
   return (
     <div className="App">
